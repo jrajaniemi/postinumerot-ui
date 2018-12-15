fad4d5c4687d6fd3d1a9631cca6713b326e62398
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2018-12-15 19:21:57 +0200

    updated

--- src/app/openapi/openapi.component.ts
+++ src/app/openapi/openapi.component.ts
@@ -1,21 +1,21 @@
-import { AfterViewInit, Component, ElementRef, OnInit } from "@angular/core"
-import SwaggerUI from "swagger-ui"
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import SwaggerUI from 'swagger-ui';
 
 @Component({
-  selector: "app-openapi",
-  templateUrl: "./openapi.component.html",
-  styleUrls: ["./openapi.component.scss"]
+  selector: 'app-openapi',
+  templateUrl: './openapi.component.html',
+  styleUrls: ['./openapi.component.scss']
 })
 export class OpenapiComponent implements AfterViewInit, OnInit {
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
     const ui = SwaggerUI({
-      url: "https://www.postinumerot.com/postalcodes.json",
-      domNode: this.el.nativeElement.querySelector(".OA-container"),
+      url: 'https://www.postinumerot.com/postalcodes.json',
+      domNode: this.el.nativeElement.querySelector('.OA-container'),
       deepLinking: true,
       presets: [SwaggerUI.presets.apis]
-    })
+    });
   }
 
   ngOnInit() {}
