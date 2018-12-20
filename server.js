8300dad6b5118c8fb7c0e48448bbc489f482607f
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2018-12-20 15:51:22 +0200

    cors added

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // Alusta express server
 const express = require('express');
 const path = require('path');
-
+var cors = require('cors');
 const app = express();
 
 // jaellaan staattisia tiedostoja ./dist/ng-postinumerot/ -kansiosta
 app.use(express.static('./dist/ng-postinumerot'));
+app.use(cors());
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-res.sendFile(path.join(__dirname,'/dist/ng-postinumerot/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/ng-postinumerot/index.html'));
 });
 
 // laitetaan 8080-portti kuunteluun
