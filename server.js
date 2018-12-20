d718983f6337532c11401c6df6e4982f10b099a3
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2018-12-20 21:01:03 +0200

    server updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 // jaellaan staattisia tiedostoja ./dist/ng-postinumerot/ -kansiosta
 app.use(express.static('./dist/ng-postinumerot'));
@@ -14,6 +15,7 @@ app.use(cors());
 
 // HTTP -> HTTPS 301
 app.use((req, res, next) => {
+  console.log(req.header);
   if (req.header('x-forwarded-proto') !== 'https')
     res.redirect(`https://${req.header('host')}${req.url}`)
   else
@@ -23,12 +25,17 @@ app.use((req, res, next) => {
 // Tarjoillaan postalcodes.json
 app.get('/postalcodes.json', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/ng-postinumerot/postalcodes.json'));
+  console.log('postalcodes ' + Date.now());
 });
 
 // Tarjoillaan aina index.html
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/ng-postinumerot/index.html'));
+  console.log('index ' + Date.now());
 });
 
 // laitetaan 8080-portti kuunteluun
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+  // Success callback
+  console.log(`Listening at http://localhost:${port}/`);
+});
